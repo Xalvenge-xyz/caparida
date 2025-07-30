dbf6c458d4683332ea5a6671f59ae8222c4b44c3
Xalvenge-xyz <mc37noob@gmail.com>
2025-07-30 20:46:52 +0800

    LATE UPDATE!

--- src/activity1/Activity1.java
+++ src/activity1/Activity1.java
@@ -1,21 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package activity1;
 
-/**
- *
- * @author PC 18
- */
+package activity1;
+import java.util.Scanner;
+
 public class Activity1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+      
+      String name;
+      double ave;
+      int subjects, total;
+      int sub1;
+      int sub2;
+      int sub3;
+ 
+      
+      System.out.print("Enter Student Name: ");
+      name = sc.nextLine();
+      
+      System.out.print("Enter number of subjects: ");
+      subjects = sc.nextInt();
+      
+      System.out.print("Enter grade for sub1: ");
+      sub1 = sc.nextInt();
+      
+      System.out.print("Enter grade for sub2: ");
+      sub2 = sc.nextInt();
+      
+      System.out.print("Enter grade for sub3: ");
+      sub3 = sc.nextInt();
+      
+      
+      total = sub1 + sub2 + sub3;
+      ave = total / subjects;
+      
+      
+      for(int i = 0; i <= subjects; i++){
+         
+         }
+      
+      System.out.printf("Average: %.2f", ave);
+      if(ave >= 75){
+        System.out.print("\nREMARKS: PASSED");
+    }else {
+          System.out.print("\nREMARKS: FAIL");
+      }
     }
     
 }
