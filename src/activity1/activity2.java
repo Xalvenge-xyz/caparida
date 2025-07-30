024988ff1fa82b5ffe107b072b5c759e2238c4c1
Xalvenge-xyz <mc37noob@gmail.com>
2025-07-30 21:18:27 +0800

    Activity2

--- src/activity1/activity2.java
+++ src/activity1/activity2.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package activity1;
 
-/**
- *
- * @author PC 18
- */
+import java.util.Scanner;
+
 public class activity2 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String name;
+        float hr;
+        int th;
+
+        System.out.print("Enter employee name: ");
+        name = input.nextLine();
+
+        System.out.print("Enter hour rate: ");
+        hr = input.nextInt();
+        
+        System.out.print("Enter total hours worked this week: ");
+        th = input.nextInt();
+        
+        float totalrate = hr * th;
+        float sss = totalrate * 0.1f;
+        float nw = totalrate - sss;
+        
+        System.out.print("-------Wage Summary--------");
+        
+        System.out.print("Employee: " +name);
+        System.out.printf("\nHourly Rate: ₱%.2f", hr);
+        System.out.print("\nHours Worked: " +th);
+        System.out.printf("\nGross Weekly Wage: ₱%.2f", totalrate);
+        System.out.printf("\nSSS Contribution (10%%): ₱%.2f", sss);
+        System.out.print("\n---------------------------");
+        System.out.printf("\nNet Weekly Wage: ₱%.2f", nw);
+        
+    }
 }
